6e2d105c148fbe82a071b5ebad6385237703c8a7
Cauã Rebelo <cauarebelo@discente.ufg.br>
2025-04-06 16:29:43 -0300

    Erros corrigidos

--- Assets/Scripts/DialogoComecoTimers.cs
+++ Assets/Scripts/DialogoComecoTimers.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class DialogoComecoTimers : MonoBehaviour
@@ -51,11 +53,36 @@ public class DialogoComecoTimers : MonoBehaviour
         paciente.SetActive(true);
     }
 
-    public void On()
+    public void OnPatientExit()
     {
-        monitor.SetActive(false);
-        counter.SetActive(false);
-        diretor.SetActive(true);
-        paciente.SetActive(true);
+        panel.SetActive(true);
+        dialogo.transform.gameObject.SetActive(true);
+        StartCoroutine(Vitoria());
+    }
+
+    public IEnumerator Vitoria()
+    {
+        dialogo.text = "Depois que o paciente saiu da sala, fui parabenizado com uma musica animada e alguns brindes...";
+        yield return new WaitForSeconds(2f);
+        dialogo.text = "Apesar da comemoração, não parecia algo feliz, parecia algo vazio... mas a recompensa podia me sustentar por enquanto.";
+        yield return new WaitForSeconds(2f);
+        dialogo.text = "E afinal de contas... Um médico sempre está ajudando os outros, certo?";
+        yield return new WaitForSeconds(2f);
+        dialogo.text = "C e R t O?";
+        yield return new WaitForSeconds(0.3f);
+        SceneManager.LoadScene("DayOne");
+    }
+
+    public IEnumerator Derrota()
+    {
+        dialogo.text = "Depois que o paciente saiu da sala, fui alertado que não foi o diagnostico certo...";
+        yield return new WaitForSeconds(2f);
+        dialogo.text = "Apesar disso, ainda recebi uma recompensa que podia me sustentar por um bom tempo.";
+        yield return new WaitForSeconds(2f);
+        dialogo.text = "Talvez isso possa funcionar... Um médico sempre está ajudando os outros, certo?";
+        yield return new WaitForSeconds(2f);
+        dialogo.text = "C e R t O?";
+        yield return new WaitForSeconds(0.3f);
+        SceneManager.LoadScene("DayOne");
     }
 }
