b01034aabb01e095b7a4ffc5dcf4a9d50352bf16
Nightfox63 <206326357+Nightfox63@users.noreply.github.com>
2025-04-05 21:08:30 -0300

    Atualização

---
+++ Assets/Scripts/Menu/MenuPrincipalManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoesDoSom;
+    [SerializeField] private GameObject painelDeOpcoesDeIdioma;
+    [SerializeField] private GameObject MensagemDeDesenvolvilmento;
+    public void Jogar()
+    {
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
+    }
+
+    public void AbrirOpcoesDoSom()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpcoesDoSom.SetActive(true);
+    }
+
+    public void FecharOpcoesDosom()
+    {
+        painelOpcoesDoSom.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
+    public void AbrirOpcoesDeIdioma()
+    {
+        painelMenuInicial.SetActive(false);
+        painelDeOpcoesDeIdioma.SetActive(true);
+    }
+
+    public void FecharOpcoesDeIdioma()
+    {
+        painelDeOpcoesDeIdioma.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
+
+    public void AbrirMensagemDeDesenvolvimento()
+    {
+        painelDeOpcoesDeIdioma.SetActive(false);
+        MensagemDeDesenvolvilmento.SetActive(true);
+    }
+
+    public void FecharMensagemDeDesenvolvimento()
+    {
+        painelDeOpcoesDeIdioma.SetActive(true);
+        MensagemDeDesenvolvilmento.SetActive(false);
+    }
+    public void SairJogo()
+    {
+        Debug.Log("Sair do Jogo");
+        Application.Quit();
+    }
+}
